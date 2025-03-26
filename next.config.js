ee603d39225da13a7d2f6d56a5dbcb42615ce436
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-27 02:23:15 +0530

    out new

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    output: 'export', // if you are using static export
-  };
-  
-  module.exports = nextConfig;
-  
