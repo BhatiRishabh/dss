023b59bd150a423b6390254e536dcbc454184479
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-27 02:48:47 +0530

    god knows

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withFlowbiteReact = require("flowbite-react/plugin/nextjs");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: 'export', // Enable static export
+  // basePath: '/dss',  // Add this line
+  experimental: {
+    appDir: true,  // Enable if you're using App router
+  },
+  trailingSlash: true, // Optional: ensures all routes have a trailing slash in the URL
+    images: {
+      unoptimized: true,
+      },
+}
+
+module.exports = withFlowbiteReact(nextConfig)
