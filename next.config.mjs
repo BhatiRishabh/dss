9c118c121751066e1695e0b35d24e36cf095d7fe
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-27 02:29:56 +0530

    dss out

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enable static export
+  basePath: '/dss',  // Add this line
   trailingSlash: true, // Optional: ensures all routes have a trailing slash in the URL
     images: {
       unoptimized: true,
