a11d2b07117f4ab8eabfa3306ee9dcdc5417bfc2
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-27 02:09:44 +0530

    new out

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
-const withFlowbiteReact = require("flowbite-react/plugin/nextjs");
+// next.config.js
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Enable static export
-  reactStrictMode: true, // Enable React Strict Mode
-
-  images: {
-    unoptimized: true,
-  },
-
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "encrypted-tbn0.gstatic.com",
-  //       pathname: "/**", // Allow all images from this domain
-  //     },
-  //   ],
-  // },
-};
-
-module.exports = withFlowbiteReact(nextConfig);
+    output: 'export', // Enable static export
+    reactStrictMode: true, // Enable React Strict Mode
+  
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
