a1b301d749152049af948ef0c7b16c75e0cc09d3
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-29 00:47:08 +0530

    Map & NAv Fixes

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ import { FiMenu, FiArrowRight } from "react-icons/fi";
 import Image from "next/image";
 const Nav = () => {
   return (
-    <div className="bg-gray-50">
+    <div className="bg-gray-50 ">
       <FlipNav />
     </div>
   );
@@ -15,7 +15,7 @@ const Nav = () => {
 const FlipNav = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className="bg-white p-4 border-b-[1px] border-gray-200 flex items-center justify-between relative">
+    <nav className="bg-white sm:p-4 p-2 border-b-[1px] border-gray-200 flex items-center justify-between relative">
       <NavLeft setIsOpen={setIsOpen} />
       <NavRight />
       <NavMenu isOpen={isOpen} />
@@ -87,7 +87,7 @@ const NavRight = () => {
       <motion.button
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
-        className="px-4 py-2 bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent font-medium rounded-md whitespace-nowrap"
+        className="px-4 py-2 hidden sm:block bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent font-medium rounded-md whitespace-nowrap"
       >
         Reach out
       </motion.button>
