3613a78092a45347b07c800b771760f18c3e48a8
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-04-01 13:00:06 +0530

    fix about height

--- src/components/Aboutus.tsx
+++ src/components/Aboutus.tsx
@@ -5,7 +5,7 @@ const Aboutus = () => {
   return (
     <div>
         
-    <div className="w-full h-[90vh] bg-slate-50">
+    <div className="w-full sm:h-[90vh] h-[110vh] bg-slate-50">
     <div className="md:h-1/4 sm:h-[20%] h-[16%] md:w-[80%] sm:w-[90%] mx-auto bg-indigo-200"></div>
     <div className="relative md:w-[70%] w-[80%] mx-auto ">
       <div className="absolute md:-top-[7rem] -top-[4rem] flex flex-col items-center">
@@ -13,7 +13,7 @@ const Aboutus = () => {
         <h2 className="sm:text-6xl md:text-7xl text-5xl pt-5 font-black text-center">About DSS&apos;25</h2>
         <p className="sm:text-center text-justify text-md mt-6">The Design Summer School (DSS) 2025 offers a five-day interactive workshop on design and related fields, including Human-Centered AI, Sensing, Photography, and AR/VR/XR. Carefully curated for students and professionals, this program provides an opportunity to expand concepts and skills through hands-on tutorials and explorations of contemporary tools and techniques. DSS aims to foster learning and growth through expert-led mentoring sessions and serves as a platform for bringing together the brightest minds for productive networking.</p>
         <Link href="https://forms.gle/qyqmH4FjPRoHP4iw8" target="_blank">
-          <button className=" rounded-xl bg-indigo-500 px-4 py-2 text-2xl font-extrabold uppercase text-neutral-50 shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-2xl my-10">
+          <button className=" rounded-xl bg-indigo-500 px-4 py-2 text-2xl font-extrabold uppercase text-neutral-50 shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-2xl mt-3">
   Register
 </button>
 </Link>
