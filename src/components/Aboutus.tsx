7c89b65c32254795f8b22912866c6d18070a93c1
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-04-01 11:55:44 +0530

    About Fix

--- src/components/Aboutus.tsx
+++ src/components/Aboutus.tsx
@@ -10,8 +10,8 @@ const Aboutus = () => {
     <div className="relative md:w-[70%] w-[80%] mx-auto ">
       <div className="absolute md:-top-[7rem] -top-[4rem] flex flex-col items-center">
         <img  className="md:w-[50rem] sm:w-[26rem] w-[20rem] object-center" src="/carousel/10.jpg" alt="Profile"/>
-        <h2 className="sm:text-5xl text-3xl font-Helvetica mt-10 text-center">About DSS&apos;25</h2>
-        <p className="sm:justify text-center text-md mt-6">The Design Summer School (DSS) 2025 offers a five-day interactive workshop on design and related fields, including Human-Centered AI, Sensing, Photography, and AR/VR/XR. Carefully curated for students and professionals, this program provides an opportunity to expand concepts and skills through hands-on tutorials and explorations of contemporary tools and techniques. DSS aims to foster learning and growth through expert-led mentoring sessions and serves as a platform for bringing together the brightest minds for productive networking.</p>
+        <h2 className="sm:text-6xl md:text-7xl text-5xl pt-5 font-black text-center">About DSS&apos;25</h2>
+        <p className="sm:text-center text-justify text-md mt-6">The Design Summer School (DSS) 2025 offers a five-day interactive workshop on design and related fields, including Human-Centered AI, Sensing, Photography, and AR/VR/XR. Carefully curated for students and professionals, this program provides an opportunity to expand concepts and skills through hands-on tutorials and explorations of contemporary tools and techniques. DSS aims to foster learning and growth through expert-led mentoring sessions and serves as a platform for bringing together the brightest minds for productive networking.</p>
         <Link href="https://forms.gle/qyqmH4FjPRoHP4iw8" target="_blank">
           <button className="pointer-events-auto rounded-xl bg-indigo-500 px-4 py-2 text-2xl font-extrabold uppercase text-neutral-50 shadow-lg transition-all duration-300 hover:scale-105 hover:shadow-2xl active:scale-100 mt-4">
   Register
