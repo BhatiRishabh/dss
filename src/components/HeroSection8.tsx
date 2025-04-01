cfcd53d91e3ff2e0efc0b1af96d868587375ce70
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-04-01 12:51:29 +0530

    Hero fixes Height

--- src/components/HeroSection8.tsx
+++ src/components/HeroSection8.tsx
@@ -49,7 +49,8 @@ export const HeroSection = () => {
       <div className="bg-slate-50">
         <div
           ref={scope}
-          className="grid h-screen w-full grid-cols-[repeat(auto-fit,_minmax(75px,_1fr))] grid-rows-[repeat(auto-fit,_minmax(75px,_1fr))]"
+          className="grid h-[120vh] w-full grid-cols-[repeat(auto-fit,_minmax(75px,_1fr))] grid-rows-[repeat(auto-fit,_minmax(75px,_1fr))]"
+
         >
           {[...Array(size.rows * size.columns)].map((_, i) => (
             <div
@@ -57,11 +58,11 @@ export const HeroSection = () => {
               id={`square-${i}`}
               // onMouseLeave={handleMouseLeave}
               // onMouseEnter={handleMouseEnter}
-              className="h-auto w-full border-[1px] border-slate-150"
+              className="h-auto w-full border-[1px]  border-slate-150"
             />
           ))}
         </div>
-        <div className="pointer-events-none absolute inset-0 flex flex-col items-center justify-center p-8">
+        <div className=" pt-[20vh] pointer-events-none absolute inset-0 flex flex-col items-center justify-center p-8">
         <Image alt="hcd"src="/logos/Logo.png" width="600"height="600"/>
           {/* <div className="flex sm:flex-row flex-col"><Image alt="hcd"src="/logos/3.svg" width="300"height="300"/>
           <Image alt="hcd"src="/logos/1.svg" width="150"height="150"/>
