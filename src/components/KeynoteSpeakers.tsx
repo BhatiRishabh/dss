b1f8d6fcc10235154a6ffd8614ae763bdc1e8d75
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-04-20 18:02:23 +0530

    Keynote Speaker fix + out

--- src/components/KeynoteSpeakers.tsx
+++ src/components/KeynoteSpeakers.tsx
@@ -10,17 +10,17 @@ const KeynoteSpeakers = () => {
     <div className="w-full">
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
         <div className="text-center pb-12">
-        <h2 className="sm:text-6xl md:text-7xl text-5xl pt-10 font-black text-center">Keynote Speakers</h2>
+        <h2 className="sm:text-6xl md:text-7xl text-5xl pt-10 font-black text-center">Keynote Speaker</h2>
 
           {/* <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-white">
             Check our awesome team members
           </h1> */}
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <div className="grid ">
           {speakers.map((speaker, index) => (
             <div key={index} className="w-full bg-gray-900 rounded-lg shadow-lg overflow-hidden flex flex-col md:flex-row">
-              <div className="w-full md:w-2/5 h-80">
+              <div className="w-full md:w-2/5 h-84">
                 <Image
                   src={speaker.image}
                   width={500}
@@ -30,9 +30,9 @@ const KeynoteSpeakers = () => {
                 />
               </div>
               <div className="w-full md:w-3/5 text-left p-6 md:p-4 space-y-2">
-                <p className="text-xl text-white font-bold">{speaker.name}</p>
-                <p className="text-base text-gray-400 font-normal">{speaker.role}</p>
-                <p className="text-base leading-relaxed text-gray-500 font-normal">{speaker.bio}</p>
+                <p className="text-4xl sm:pt-24 text-white font-bold">{speaker.name}</p>
+                <p className="text-2xl text-gray-400 font-normal">{speaker.role}</p>
+                <p className="text-xl leading-relaxed text-gray-500 font-normal">{speaker.bio}</p>
 
                 <div className="flex justify-start space-x-4 pt-2">
                   {speaker.linkedin && (
