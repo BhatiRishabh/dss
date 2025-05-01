7fb3fb06e59861e5631dde0ebeaac532a43b3b8f
BhatiRishabh <rishabhbhati831@gmail.com>
2025-05-01 22:34:25 +0530

    Add workshop details for Bijendra Kumar in EventCard component

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -62,6 +62,50 @@ const EventCard = () => {
     </motion.div>
     
     </div>
+    <div className="flex flex-col px-10 sm:px-60 py-10 space-y-2">
+     
+    <motion.div
+      className="flex flex-col md:flex-row items-center bg-indigo-400  rounded-xl border border-transparent dark:border-white/[0.2] shadow-lg overflow-hidden p-4 md:p-6 relative"
+      whileHover={{ scale: 1.03 }}
+      whileTap={{ scale: 0.98 }}
+    >
+      {/* Image Section */}
+      <div className="flex-shrink-0 flex justify-center items-center">
+        <Image
+          src="/img/Bijendra.png"
+          alt="Founder"
+          width={200}
+          height={250}
+          className="rounded-xl border-4 border-white shadow-lg"
+          priority
+        />
+      </div>
+
+      {/* Content Section */}
+      <div className="p-4 flex flex-col justify-between">
+        <div>
+          <h3 className="text-xl font-bold text-white">
+          Workshop on Photography by Bijendra Kumar
+          </h3>
+          <p className="italic text-white">
+            
+            Leading Cartoonists in India
+          </p>
+          <p className="mt-3 text-sm text-white text-justify">
+          Bijendra Kumar, a seasoned photographer and visual artist, joins DSS 2025 to share his mastery in photography. 
+          With decades of experience across cultural festivals, exhibitions, and national art shows, his creative 
+          lens captures India&apos;s heritage and emotion. Participants will gain insights from his award-winning artistry 
+          and storytelling through visuals.
+          </p>
+        </div>
+       
+      </div>
+
+      {/* Button in Bottom-Right Corner */}
+      
+    </motion.div>
+    
+    </div>
     </div>
   );
 };
