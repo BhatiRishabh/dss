1c2dea3d64631eb84b6afb1468c823f1fe612029
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-07-17 01:30:43 +0530

    Update Merch

--- src/components/Merch.tsx
+++ src/components/Merch.tsx
@@ -9,16 +9,17 @@ export default function Merch() {
       <div className="flex flex-col md:flex-row items-center gap-20">
         {/* Text column */}
         <div className="md:w-1/2">
-          <h1 className="sm:text-6xl md:text-7xl text-5xl pt-10 font-black font-krona text-left">
-            <span className="bg-gradient-to-r from-[#D8ECFF] via-[#56C1FF] to-[#EA80FC] bg-clip-text text-transparent">
-              DSS 2025
-            </span>
-            <br />
-            Exclusive Merch Is Here.
-          </h1>
-          <p className="mt-4 text-lg md:text-xl">
-            Limited-edition tees, hoodies &amp; stickers crafted for devs, dreamers and doers.
-          </p>
+<h1 className="sm:text-6xl md:text-7xl text-5xl pt-10 font-black font-krona text-left">
+  <span className="bg-gradient-to-r from-[#D8ECFF] via-[#56C1FF] to-[#EA80FC] bg-clip-text text-transparent">
+    DSS 2025
+  </span>
+  <br />
+  Swag. Snacks. Vibes.
+</h1>
+<p className="mt-4 text-lg md:text-xl">
+  Celebrate DSS 2025 with exclusive merchandise and a curated selection of gourmet food, designed for developers, innovators, and visionaries alike.
+</p>
+
 
           {/* Call-to-action with scale only */}
           {/* <motion.button
@@ -43,7 +44,7 @@ export default function Merch() {
           className="w-full md:w-1/2 flex justify-center"
         >
           <Image
-            src="/dss_logo_final.svg"
+            src="/Merch.png"
             alt="DSS 2025 merch preview"
             width={800}
             height={600}
